a9f4820f1249092c3a77a41181f073d9c2c95275
Xiangyu Li <shoreray@live.com>
2014-12-13 10:20:57 +0800

    writing report

--- symsql/symex/fuzzy.py
+++ symsql/symex/fuzzy.py
@@ -764,45 +764,5 @@ def concolic_test(testfunc, maxiter = 1000, verbose = 0):
         checked.add(pc_to_explore)
     checked.add(pc_prefix)
 
-    ## for each branch, invoke Z3 to find an input that would go
-    ## the other way, and add it to the list of inputs to explore.
-
-    ## Exercise 3: your code here.
-    ##
-    ## Here's a possible plan of attack:
-    ##
-    ## - Iterate over the set of branches in cur_path_constr.
-    ##
-    ## - Compute an AST expression for the constraints necessary
-    ##   to go the other way on that branch.  You can use existing
-    ##   logical AST combinators like sym_not(), sym_and(), etc.
-    ##
-    ##   Note that some of the AST combinators take separate positional
-    ##   arguments. In Python, to unpack a list into separate positional
-    ##   arguments, use the '*' operator documented at
-    ##   https://docs.python.org/2/tutorial/controlflow.html#unpacking-argument-lists
-    ##
-    ## - If this constraint is already in the "checked" set, skip
-    ##   it (otherwise, add it to prevent further duplicates).
-    ##
-    ## - Invoke Z3, along the lines of:
-    ##
-    ##     (ok, model) = fork_and_check(constr)
-    ##
-    ## - If Z3 was able to find example inputs that go the other
-    ##   way on this branch, make a new input set containing the
-    ##   values from Z3's model, and add it to the set of inputs
-    ##   to consider:
-    ##
-    ##     inputs.add(new_values, caller)
-    ##
-    ##   where caller is the corresponding value from the list
-    ##   of call sites (cur_path_constr_callers).
-    ##
-    ##   Note that Z3 might not assign values to every variable,
-    ##   such as if that variable turns out to be irrelevant to
-    ##   the overall constraint, so be sure to preserve values
-    ##   from the initial input (concrete_values).
-
   if verbose > 0:
     print 'Stopping after', iter, 'iterations'
